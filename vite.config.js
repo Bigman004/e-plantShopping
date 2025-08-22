f1c9273b036bdcacec69cee35d6017fa8c2aff39
Ayomide <adedijiay123@gmail.com>
2025-08-22 20:29:47 +0100

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/https://github.com/Bigman004/e-plantShopping",
   plugins: [react()],
+  base: "/e-plantShopping",
 });
