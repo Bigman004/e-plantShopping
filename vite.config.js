04c1c1ec8fa95b9581f5826e1150087c37acbef9
Ayomide <adedijiay123@gmail.com>
2025-08-23 08:14:54 +0100

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,18 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/e-plantShopping/",
+  esbuild: {
+    resolve: {
+      alias: {
+        "./runtimeConfig": "./runtimeConfig.browser",
+      },
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        loader: {
+          ".js": "jsx",
+        },
+      },
+    },
+  },
 });
